1c3259877475b6da157a00e646369db6013d55b7
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-06-21 21:47:02 -0400

    Studio Part 2 Array and String Conversion Added

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -5,44 +5,89 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
+function checkSpecialCharacters(strings) {
+	// Check for each special character using includes method
+	const hasComma =  strings.includes(",");
+	const hasSemicolon = strings.includes(";");
+	const hasSpace = strings.includes(" ");
+	// Return true if any of the characters are found
+	return hasComma || hasSemicolon || hasSpace;
+
+  }
+
+console.log(checkSpecialCharacters(protoArray1)); 
+console.log(checkSpecialCharacters(protoArray2)); 
+console.log(checkSpecialCharacters(protoArray3)); 
+console.log(checkSpecialCharacters(protoArray4)); 
+
+
 //2) 
-function reverseCommas() {
+function reverseCommas(strings) { 
+	if (strings.includes(',')) {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
+	let check = strings.split(',');
+	let reversedArray = check.reverse();
+	let output = reversedArray.join(',');
 	//TODO: 2. write the code required for this step
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
+} 	
 }
 
 //3)
-function semiDash() {
-	let check;
-	let output;
+function semiDash(strings) {
+	if (strings.includes(';')) {
+	let check = strings.split(';');
+	check.sort();
 //TODO: write the code required for this step
-
+    let output = check.join('-');
   
 	return output;
+} else {
+	return strings;
+}
 }
 
+strings.forEach(function(strings) {
+	let result = semiDash(strings);
+	console.log(result);
+  });
+
 //4)
-function reverseSpaces() {
-	let check;
-	let output;
+function reverseSpaces(strings) {
+	if (strings.includes(' ')) {
+	  let check = strings.split(' ');
+	  check.sort().reverse();
+	  let output = check.join(' ');
   //TODO: write the code required for this step
 
 	return output;
+} else {
+	// If the string does not contain spaces, return it unchanged
+	return strings;
+  }
 }
 
+let results = strings.map(reverseSpaces);
+console.log(results); 
+
 //5)
-function commaSpace() {
-	let check;
-	let output;
+function commaSpace(strings) {
+	if (strings.includes(', ')) {
+	let check = strings.split(', ');
+	check.reverse();
+	let output = check.join(',');
 	//TODO: write the code required for this step
   
 	return output;
+} else {
+	return strings;
 }
+}
+
+let results1 = strings.map(commaSpace);
+console.log(results1); 
 
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
 module.exports = {
