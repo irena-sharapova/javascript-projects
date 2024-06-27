29d14fde23d961d70d2bf718d894907572c29db3
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-06-27 16:52:22 -0400

    Exercises Functions. Bonus mission Added

--- functions/exercises.js
+++ functions/exercises.js
@@ -1,21 +1,21 @@
 
-function makeLine(size) {
+function makeLine(size, character = '#') {
   let line = '';
   for (let i = 0; i < size; i++) {
-    line += '#';
+    line += character;
   }
   return line;
 }
-console.log(makeLine(5));
+console.log(makeLine(5, '*'));
 
 
 
 console.log('Make Square:');
-function makeSquare(size) {
+function makeSquare(size, character = '#') {
     let square = '';
   
     for (let i = 0; i < size; i++) {
-      square += makeLine(size);
+      square += makeLine(size, character);
       if (i < size - 1) {
         square += '\n';
       }
@@ -23,67 +23,67 @@ function makeSquare(size) {
   
     return square;
   }
-console.log(makeSquare(5));
+console.log(makeSquare(5, '*'));
 
 console.log('Make Rectangle:');
-function makeRectangle(width, height) {
+function makeRectangle(width, height, character = '#') {
   let rectangle = '';
   for (let i = 0; i < height; i++) {
-    rectangle += (makeLine(width) + '\n');
+    rectangle += (makeLine(width, character) + '\n');
   }
   return rectangle.slice(0, -1);
 }
-console.log(makeRectangle(5, 3));
+console.log(makeRectangle(5, 3, '*'));
 
 console.log('Make Square use Ractangle method:');
-function makeRectangle(width, height) {
+function makeRectangle(width, height, character = '#') {
   let rectangle = '';
   for (let i = 0; i < height; i++) {
-    rectangle += (makeLine(width) + '\n');
+    rectangle += (makeLine(width, character) + '\n');
   }
   return rectangle.slice(0, -1);
 }
-console.log(makeRectangle(5, 5));
+console.log(makeRectangle(5, 5, '*'));
 
 
 console.log('Make Downward Stairs:');
-function makeDownwardStairs(height) {
+function makeDownwardStairs(height, character = '#') {
   let stairs = '';
   for (let i = 0; i < height; i++) {
-    stairs += (makeLine(i+1) + '\n');
+    stairs += (makeLine(i+1, character) + '\n');
   }
   return stairs.slice(0, -1);
 }
-console.log(makeDownwardStairs(5));
+console.log(makeDownwardStairs(5, '*'));
 
 console.log('Make Spase Line:');
-function makeSpaceLine(numSpaces, numChars) {
+function makeSpaceLine(numSpaces, numChars, character = '*') {
   let spaces = ' '.repeat(numSpaces);
-  let chars = '#'.repeat(numChars);
+  let chars = character.repeat(numChars);
   let line = spaces + chars + spaces;
   return line;
 }
-console.log(makeSpaceLine(3, 5));
+console.log(makeSpaceLine(3, 5, '*'));
 
 console.log('Make Triangle:');
-function makeIsoscelesTriangle(height) {
+function makeIsoscelesTriangle(height, character = '*') {
   let triangle = '';
   for (let i = 0; i < height; i++) {
     triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
   }
   return triangle.slice(0, -1);
 }
-console.log(makeIsoscelesTriangle(5));
+console.log(makeIsoscelesTriangle(5, '*'));
 
 console.log('Make Diamonds:');
-function makeDiamond(height) {
+function makeDiamond(height, character = '*') {
   let triangle = '';
   for (let i = 0; i < height; i++) {
-    triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+    triangle += (makeSpaceLine(height - i - 1, 2*i + 1, character) + '\n');
   }
   for (let i = height -2; i >= 0; i--) {
-    triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+    triangle += (makeSpaceLine(height - i - 1, 2*i + 1, character) + '\n');
   }
   return triangle.slice(0, -1);
 }
-console.log(makeDiamond(5));
+console.log(makeDiamond(5, '*'));
