cb3a3b27b52c5612e03acc8c84989c138770bb4b
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-08-04 23:23:14 -0400

    add exercises DOM and Event

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,9 +2,24 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
+    const abortButton = document.getElementById("abortMission")
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+
+     abortButton.addEventListener('click', function() {
+        const userConfirmed = confirm('Are you sure you want to abort the mission?');
+
+        if (userConfirmed) {
+            statusElement.textContent = 'Mission aborted! Space shuttle returning home.';
+        } 
+    });
 }
 
 window.addEventListener("load", init);
